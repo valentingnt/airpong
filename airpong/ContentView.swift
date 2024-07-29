5baa44baa88215f5d2b17f108c8a1d1073eff07e
Valentin Genest <valentin64.genest@gmail.com>
2024-07-29 09:15:26 +0200

    Initial Commit

---
+++ airpong/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  airpong
+//
+//  Created by Valentin Genest on 29/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ airpong/airpongApp.swift
@@ -0,0 +1,17 @@
+//
+//  airpongApp.swift
+//  airpong
+//
+//  Created by Valentin Genest on 29/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct airpongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
